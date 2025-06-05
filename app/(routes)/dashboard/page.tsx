b6587aa5e00a8cad77aa0df5a024552f32f8cccb
eduardo <eduardomendes418@gmail.com>
2025-06-05 16:18:19 -0300

    [AICA-07] feat:  AIHistory

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import WelcomeBanner from "./_components/Welcome/WelcomeBanner";
 import AITools from "./_components/Tools/AITools";
+import AIHistory from "./_components/History/AIHistory";
 
 function Dashboard() {
   return (
     <div>
       <WelcomeBanner />
       <AITools/>
+      <AIHistory/>
     </div>
   );
 }
